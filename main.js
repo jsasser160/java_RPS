73e01ffaa9bf4fceb637438762fb654f6eaf6065
natalie evans <nmevans7@gmail.com>
2020-06-11 18:50:41 -0600

    initial

---
+++ main.js
@@ -0,0 +1,23 @@
+// Get user guess, rock paper or scissors
+// Get computer guess, rock paper or scissors
+// Compare the guess
+// Pick a winner
+// Display the winner
+
+
+
+// Variables
+const player;
+// const name = document.getElementById('name');
+// const startButton = documnet.getElementById('start-button');
+
+// Fuctions
+
+// function calcAverage() {
+  // return (Math.floor((wins/games)))
+// }
+
+function startGame() {
+  const playerChoice =[ rock, paper, scissor]
+}
+
